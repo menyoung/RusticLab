f1dced3a7de461bfbeb21677db01aa5d952f8aeb
menyoung <menyounglee@gmail.com>
2013-12-30 12:01:40 -0800

    made visa into a proper library"

--- ex3.rs
+++ ex3.rs
@@ -1,18 +1,15 @@
 #[feature(globs)];
-#[allow(dead_code)];
-use visatype::*;
-use visadef::*;
-mod visatype;
-mod visadef;
-mod visafn;
-mod visa;
+
+extern mod visa;
+
+use visa::*;
 
 fn main() {
 	println(format!("Hello. VISA Version is {}.{}.{}.", VI_VERSION_MAJOR(VI_SPEC_VERSION) as int, VI_VERSION_MINOR(VI_SPEC_VERSION) as int, VI_VERSION_SUBMINOR(VI_SPEC_VERSION) as int));
 	
 	// initialize the system.
 	let mut defaultRM: ViSession = ViSession(25813);
-	match visa::open_default_RM() {
+	match vi_open_default_RM() {
 		(status, resource_manager) => {
 			*defaultRM = *resource_manager;
 			println(format!("Default Resource Manager is {}; Status = {}.", *defaultRM as int, *status as int));
@@ -26,7 +23,7 @@ fn main() {
 	// open communication with gpib device at primary address 12.
 	// no error checking!
 	let mut instr: ViSession = ViSession(25814);
-	match visa::open(defaultRM, "GPIB0::12::INSTR", ViAccessMode(0), 0) {
+	match vi_open(defaultRM, "GPIB0::12::INSTR", ViAccessMode(0), 0) {
 		(status, vi) => {
 			*instr = *vi;
 			println(format!("Instrument at address 12 is {}; Status = {}.", *instr as int, *status as int));
@@ -38,12 +35,12 @@ fn main() {
 	}
 	
 	// set the timeout for messages
-	visa::set_attribute(ViObject(*instr), ViAttr(VI_ATTR_TMO_VALUE), ViAttrState(5000));
+	vi_set_attribute(ViObject(*instr), ViAttr(VI_ATTR_TMO_VALUE), ViAttrState(5000));
 	// clear the instrument
-	visa::clear(instr);
+	vi_clear(instr);
 
 	// ask the device for identification
-	match visa::write_str(instr, "*IDN?\n") {
+	match vi_write_str(instr, "*IDN?\n") {
 		(status, retCnt) => {
 			println(format!("Upon write operation, RetCount = {}; Status = {}.", retCnt, *status as int));
 			if (*status < VI_SUCCESS) {
@@ -55,7 +52,7 @@ fn main() {
 	
 	// read the response
 	let MAX_CNT = 200;
-	match visa::read_str(instr, MAX_CNT) {
+	match vi_read_str(instr, MAX_CNT) {
 		(status, msg, retCnt) => {
 			println(msg);
 			println(format!("Message was {} bytes long. Status = {}.", retCnt, *status as int));
@@ -66,8 +63,8 @@ fn main() {
 		}
 	}
 
-	visa::write_str(instr, "MEAS:VOLT:DC?");
-	match visa::read_str(instr, MAX_CNT) {
+	vi_write_str(instr, "MEAS:VOLT:DC?");
+	match vi_read_str(instr, MAX_CNT) {
 		(status, msg, retCnt) => {
 			println(msg);
 			println(format!("Message was {} bytes long. Status = {}.", retCnt, *status as int));
@@ -78,6 +75,6 @@ fn main() {
 		}
 	}
 
-	visa::close(instr);
-	visa::close(defaultRM);
+	vi_close(instr);
+	vi_close(defaultRM);
 }
--- visa.rs
+++ visa.rs
@@ -1,37 +1,43 @@
-use visatype::*;
-use visadef::*;
-use visafn::*;
+#[crate_id = "visa#0.1"];
+#[crate_type = "lib"];
+
+#[feature(globs)];
+#[allow(dead_code)];
+
+pub use visatype::*;
+pub use visadef::*;
+pub use visafn::*;
 mod visatype;
 mod visadef;
 mod visafn;
 
-pub fn open_default_RM() -> (ViStatus, ViSession) {
+pub fn vi_open_default_RM() -> (ViStatus, ViSession) {
 	let mut status: ViStatus = ViStatus(_VI_ERROR);
 	let mut resource_manager: ViSession = ViSession(0);
 	unsafe { *status = viOpenDefaultRM(&mut *resource_manager) };
 	(status, resource_manager)
 }
-pub fn open(rsrc_mngr: ViSession, name: &str, mode: ViAccessMode, timeout: uint) -> (ViStatus, ViSession) {
+pub fn vi_open(rsrc_mngr: ViSession, name: &str, mode: ViAccessMode, timeout: uint) -> (ViStatus, ViSession) {
 	let mut status: ViStatus = ViStatus(_VI_ERROR);
 	let mut instr: ViSession = ViSession(0);
 	unsafe { *status = viOpen(*rsrc_mngr, name.to_c_str().unwrap(), *mode, timeout as u32, &mut *instr) };
 	(status, instr)
 }
-pub fn set_attribute(vi: ViObject, attrName: ViAttr, attrValue: ViAttrState) -> ViStatus {
+pub fn vi_set_attribute(vi: ViObject, attrName: ViAttr, attrValue: ViAttrState) -> ViStatus {
 	let mut status: ViStatus = ViStatus(_VI_ERROR);
 	unsafe { *status = viSetAttribute(*vi, *attrName, *attrValue) };
 	status
 }
-pub fn write(vi: ViSession, buf: &[u8]) -> (ViStatus, uint) {
+pub fn vi_write(vi: ViSession, buf: &[u8]) -> (ViStatus, uint) {
 	let mut status: ViStatus = ViStatus(_VI_ERROR);
 	let mut retCnt: u32 = 0;
 	unsafe { *status = viWrite(*vi, buf.as_ptr(), buf.iter().len() as u32, &mut retCnt) };
 	(status, retCnt as uint)
 }
-pub fn write_str(vi: ViSession, buf: &str) -> (ViStatus, uint) {
-	write(vi, buf.as_bytes())
+pub fn vi_write_str(vi: ViSession, buf: &str) -> (ViStatus, uint) {
+	vi_write(vi, buf.as_bytes())
 }
-pub fn read(vi: ViSession, cnt: uint) -> (ViStatus, ~[u8], uint) {
+pub fn vi_read(vi: ViSession, cnt: uint) -> (ViStatus, ~[u8], uint) {
 	let mut status: ViStatus = ViStatus(_VI_ERROR);
 	let mut buf = ::std::vec::with_capacity(cnt);
 	let mut retCnt: u32 = 0;
@@ -41,21 +47,21 @@ pub fn read(vi: ViSession, cnt: uint) -> (ViStatus, ~[u8], uint) {
 	};
 	(status, buf, retCnt as uint)
 }
-pub fn read_str(vi: ViSession, cnt: uint) -> (ViStatus, ~str, uint) {
-	match read(vi, cnt) {
+pub fn vi_read_str(vi: ViSession, cnt: uint) -> (ViStatus, ~str, uint) {
+	match vi_read(vi, cnt) {
 		(status, buf, retCnt) => {
 			(status, buf.into_ascii().into_str(), retCnt)
 		}
 	}
 }
-pub fn close(vi: ViSession) -> ViStatus {
+pub fn vi_close(vi: ViSession) -> ViStatus {
 	let mut status: ViStatus = ViStatus(_VI_ERROR);
 	unsafe {
 		*status = viClose(*vi);
 	};
 	status
 }
-pub fn clear(vi: ViSession) -> ViStatus {
+pub fn vi_clear(vi: ViSession) -> ViStatus {
 	let mut status: ViStatus = ViStatus(_VI_ERROR);
 	unsafe {
 		*status = viClear(*vi);
