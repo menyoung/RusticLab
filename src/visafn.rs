26fddffd26cae78609e1482d446bce5bffcb0ef1
Menyoung Lee <menyounglee@gmail.com>
2015-11-15 15:17:26 -0800

    start of trying to fix pointers

--- src/visafn.rs
+++ src/visafn.rs
@@ -2,12 +2,12 @@ use std::libc::c_char;
 
 // VISA functions
 // They're slowly being ported on an as-needed basis.
-// 
+//
 /*- Resource Manager Functions and Operations -------------------------------*/
 #[link(name = "VISA", kind = "framework")]
 extern {
 // ViStatus _VI_FUNC  viOpenDefaultRM (ViPSession vi);
-	pub fn viOpenDefaultRM(vi: *mut u32) -> i32;
+	pub fn viOpenDefaultRM(vi: &mut u32) -> i32;
 // ViStatus _VI_FUNC  viFindRsrc      (ViSession sesn, ViString expr, ViPFindList vi,
 //                                     ViPUInt32 retCnt, ViChar _VI_FAR desc[]);
 // ViStatus _VI_FUNC  viFindNext      (ViFindList vi, ViChar _VI_FAR desc[]);
@@ -19,10 +19,10 @@ extern {
 //                                     ViChar _VI_FAR aliasIfExists[]);
 // ViStatus _VI_FUNC  viOpen          (ViSession sesn, ViRsrc name, ViAccessMode mode,
 //                                     ViUInt32 timeout, ViPSession vi);
-	pub fn viOpen(sesn: u32, name: *c_char, mode: u32, timeout: u32, vi: *mut u32) -> i32;
+	pub fn viOpen(sesn: u32, name: *c_char, mode: u32, timeout: u32, vi: &mut u32) -> i32;
 }
 /// calls `viOpenDefaultRM(vi)`.
-pub unsafe fn viGetDefaultRM(vi: *mut u32) -> i32 { viOpenDefaultRM(vi) }
+pub fn viGetDefaultRM(vi: &mut u32) -> i32 { viOpenDefaultRM(vi) }
 
 /*- Resource Template Operations --------------------------------------------*/
 #[link(name = "VISA", kind = "framework")]
@@ -35,7 +35,7 @@ extern {
 // ViStatus _VI_FUNC  viStatusDesc    (ViObject vi, ViStatus status, ViChar _VI_FAR desc[]);
 	pub fn viStatusDesc(vi: u32, status: i32, desc: *u8) -> i32;
 // ViStatus _VI_FUNC  viTerminate     (ViObject vi, ViUInt16 degree, ViJobId jobId);
-// 
+//
 // ViStatus _VI_FUNC  viLock          (ViSession vi, ViAccessMode lockType, ViUInt32 timeout,
 //                                     ViKeyId requestedKey, ViChar _VI_FAR accessKey[]);
 // ViStatus _VI_FUNC  viUnlock        (ViSession vi);
@@ -55,12 +55,12 @@ extern {
 #[link(name = "VISA", kind = "framework")]
 extern {
 // ViStatus _VI_FUNC  viRead          (ViSession vi, ViPBuf buf, ViUInt32 cnt, ViPUInt32 retCnt);
-	pub fn viRead(vi: u32, buf: *u8, cnt: u32, retCnt: *mut u32) -> i32;
+	pub fn viRead(vi: u32, buf: *u8, cnt: u32, retCnt: &mut u32) -> i32;
 // ViStatus _VI_FUNC  viReadAsync     (ViSession vi, ViPBuf buf, ViUInt32 cnt, ViPJobId  jobId);
 // ViStatus _VI_FUNC  viReadToFile    (ViSession vi, ViConstString filename, ViUInt32 cnt,
 //                                     ViPUInt32 retCnt);
 // ViStatus _VI_FUNC  viWrite         (ViSession vi, ViBuf  buf, ViUInt32 cnt, ViPUInt32 retCnt);
-	pub fn viWrite(vi: u32, buf: *u8, cnt: u32, retCnt: *mut u32) -> i32;
+	pub fn viWrite(vi: u32, buf: *u8, cnt: u32, retCnt: &mut u32) -> i32;
 // ViStatus _VI_FUNC  viWriteAsync    (ViSession vi, ViBuf  buf, ViUInt32 cnt, ViPJobId  jobId);
 // ViStatus _VI_FUNC  viWriteFromFile (ViSession vi, ViConstString filename, ViUInt32 cnt,
 //                                     ViPUInt32 retCnt);
@@ -68,33 +68,33 @@ extern {
 // ViStatus _VI_FUNC  viReadSTB       (ViSession vi, ViPUInt16 status);
 // ViStatus _VI_FUNC  viClear         (ViSession vi);
 	pub fn viClear(vi: u32) -> i32;
-} 
+}
 /*- Formatted and Buffered I/O Operations -----------------------------------*/
-// 
+//
 // ViStatus _VI_FUNC  viSetBuf        (ViSession vi, ViUInt16 mask, ViUInt32 size);
 // ViStatus _VI_FUNC  viFlush         (ViSession vi, ViUInt16 mask);
-// 
+//
 // ViStatus _VI_FUNC  viBufWrite      (ViSession vi, ViBuf  buf, ViUInt32 cnt, ViPUInt32 retCnt);
 // ViStatus _VI_FUNC  viBufRead       (ViSession vi, ViPBuf buf, ViUInt32 cnt, ViPUInt32 retCnt);
-// 
+//
 // ViStatus _VI_FUNCC viPrintf        (ViSession vi, ViString writeFmt, ...);
 // ViStatus _VI_FUNC  viVPrintf       (ViSession vi, ViString writeFmt, ViVAList params);
 // ViStatus _VI_FUNCC viSPrintf       (ViSession vi, ViPBuf buf, ViString writeFmt, ...);
 // ViStatus _VI_FUNC  viVSPrintf      (ViSession vi, ViPBuf buf, ViString writeFmt,
 //                                     ViVAList parms);
-// 
+//
 // ViStatus _VI_FUNCC viScanf         (ViSession vi, ViString readFmt, ...);
 // ViStatus _VI_FUNC  viVScanf        (ViSession vi, ViString readFmt, ViVAList params);
 // ViStatus _VI_FUNCC viSScanf        (ViSession vi, ViBuf buf, ViString readFmt, ...);
 // ViStatus _VI_FUNC  viVSScanf       (ViSession vi, ViBuf buf, ViString readFmt,
 //                                     ViVAList parms);
-// 
+//
 // ViStatus _VI_FUNCC viQueryf        (ViSession vi, ViString writeFmt, ViString readFmt, ...);
-// ViStatus _VI_FUNC  viVQueryf       (ViSession vi, ViString writeFmt, ViString readFmt, 
+// ViStatus _VI_FUNC  viVQueryf       (ViSession vi, ViString writeFmt, ViString readFmt,
 //                                     ViVAList params);
-// 
+//
 /*- Memory I/O Operations ---------------------------------------------------*/
-// 
+//
 // ViStatus _VI_FUNC  viIn8           (ViSession vi, ViUInt16 space,
 //                                     ViBusAddress offset, ViPUInt8  val8);
 // ViStatus _VI_FUNC  viOut8          (ViSession vi, ViUInt16 space,
@@ -107,13 +107,13 @@ extern {
 //                                     ViBusAddress offset, ViPUInt32 val32);
 // ViStatus _VI_FUNC  viOut32         (ViSession vi, ViUInt16 space,
 //                                     ViBusAddress offset, ViUInt32  val32);
-// 
+//
 // #if defined(_VI_INT64_UINT64_DEFINED)
 // ViStatus _VI_FUNC  viIn64          (ViSession vi, ViUInt16 space,
 //                                     ViBusAddress offset, ViPUInt64 val64);
 // ViStatus _VI_FUNC  viOut64         (ViSession vi, ViUInt16 space,
 //                                     ViBusAddress offset, ViUInt64  val64);
-// 
+//
 // ViStatus _VI_FUNC  viIn8Ex         (ViSession vi, ViUInt16 space,
 //                                     ViBusAddress64 offset, ViPUInt8  val8);
 // ViStatus _VI_FUNC  viOut8Ex        (ViSession vi, ViUInt16 space,
@@ -131,7 +131,7 @@ extern {
 // ViStatus _VI_FUNC  viOut64Ex       (ViSession vi, ViUInt16 space,
 //                                     ViBusAddress64 offset, ViUInt64  val64);
 // #endif
-// 
+//
 // ViStatus _VI_FUNC  viMoveIn8       (ViSession vi, ViUInt16 space, ViBusAddress offset,
 //                                     ViBusSize length, ViAUInt8  buf8);
 // ViStatus _VI_FUNC  viMoveOut8      (ViSession vi, ViUInt16 space, ViBusAddress offset,
@@ -144,13 +144,13 @@ extern {
 //                                     ViBusSize length, ViAUInt32 buf32);
 // ViStatus _VI_FUNC  viMoveOut32     (ViSession vi, ViUInt16 space, ViBusAddress offset,
 //                                     ViBusSize length, ViAUInt32 buf32);
-// 
+//
 // #if defined(_VI_INT64_UINT64_DEFINED)
 // ViStatus _VI_FUNC  viMoveIn64      (ViSession vi, ViUInt16 space, ViBusAddress offset,
 //                                     ViBusSize length, ViAUInt64 buf64);
 // ViStatus _VI_FUNC  viMoveOut64     (ViSession vi, ViUInt16 space, ViBusAddress offset,
 //                                     ViBusSize length, ViAUInt64 buf64);
-// 
+//
 // ViStatus _VI_FUNC  viMoveIn8Ex     (ViSession vi, ViUInt16 space, ViBusAddress64 offset,
 //                                     ViBusSize length, ViAUInt8  buf8);
 // ViStatus _VI_FUNC  viMoveOut8Ex    (ViSession vi, ViUInt16 space, ViBusAddress64 offset,
@@ -168,73 +168,73 @@ extern {
 // ViStatus _VI_FUNC  viMoveOut64Ex   (ViSession vi, ViUInt16 space, ViBusAddress64 offset,
 //                                     ViBusSize length, ViAUInt64 buf64);
 // #endif
-// 
+//
 // ViStatus _VI_FUNC  viMove          (ViSession vi, ViUInt16 srcSpace, ViBusAddress srcOffset,
-//                                     ViUInt16 srcWidth, ViUInt16 destSpace, 
-//                                     ViBusAddress destOffset, ViUInt16 destWidth, 
-//                                     ViBusSize srcLength); 
+//                                     ViUInt16 srcWidth, ViUInt16 destSpace,
+//                                     ViBusAddress destOffset, ViUInt16 destWidth,
+//                                     ViBusSize srcLength);
 // ViStatus _VI_FUNC  viMoveAsync     (ViSession vi, ViUInt16 srcSpace, ViBusAddress srcOffset,
-//                                     ViUInt16 srcWidth, ViUInt16 destSpace, 
-//                                     ViBusAddress destOffset, ViUInt16 destWidth, 
+//                                     ViUInt16 srcWidth, ViUInt16 destSpace,
+//                                     ViBusAddress destOffset, ViUInt16 destWidth,
 //                                     ViBusSize srcLength, ViPJobId jobId);
-// 
+//
 // #if defined(_VI_INT64_UINT64_DEFINED)
 // ViStatus _VI_FUNC  viMoveEx        (ViSession vi, ViUInt16 srcSpace, ViBusAddress64 srcOffset,
-//                                     ViUInt16 srcWidth, ViUInt16 destSpace, 
-//                                     ViBusAddress64 destOffset, ViUInt16 destWidth, 
-//                                     ViBusSize srcLength); 
+//                                     ViUInt16 srcWidth, ViUInt16 destSpace,
+//                                     ViBusAddress64 destOffset, ViUInt16 destWidth,
+//                                     ViBusSize srcLength);
 // ViStatus _VI_FUNC  viMoveAsyncEx   (ViSession vi, ViUInt16 srcSpace, ViBusAddress64 srcOffset,
-//                                     ViUInt16 srcWidth, ViUInt16 destSpace, 
-//                                     ViBusAddress64 destOffset, ViUInt16 destWidth, 
+//                                     ViUInt16 srcWidth, ViUInt16 destSpace,
+//                                     ViBusAddress64 destOffset, ViUInt16 destWidth,
 //                                     ViBusSize srcLength, ViPJobId jobId);
 // #endif
-// 
+//
 // ViStatus _VI_FUNC  viMapAddress    (ViSession vi, ViUInt16 mapSpace, ViBusAddress mapOffset,
 //                                     ViBusSize mapSize, ViBoolean access,
 //                                     ViAddr suggested, ViPAddr address);
 // ViStatus _VI_FUNC  viUnmapAddress  (ViSession vi);
-// 
+//
 // #if defined(_VI_INT64_UINT64_DEFINED)
 // ViStatus _VI_FUNC  viMapAddressEx  (ViSession vi, ViUInt16 mapSpace, ViBusAddress64 mapOffset,
 //                                     ViBusSize mapSize, ViBoolean access,
 //                                     ViAddr suggested, ViPAddr address);
 // #endif
-// 
+//
 // void     _VI_FUNC  viPeek8         (ViSession vi, ViAddr address, ViPUInt8  val8);
 // void     _VI_FUNC  viPoke8         (ViSession vi, ViAddr address, ViUInt8   val8);
 // void     _VI_FUNC  viPeek16        (ViSession vi, ViAddr address, ViPUInt16 val16);
 // void     _VI_FUNC  viPoke16        (ViSession vi, ViAddr address, ViUInt16  val16);
 // void     _VI_FUNC  viPeek32        (ViSession vi, ViAddr address, ViPUInt32 val32);
 // void     _VI_FUNC  viPoke32        (ViSession vi, ViAddr address, ViUInt32  val32);
-// 
+//
 // #if defined(_VI_INT64_UINT64_DEFINED)
 // void     _VI_FUNC  viPeek64        (ViSession vi, ViAddr address, ViPUInt64 val64);
 // void     _VI_FUNC  viPoke64        (ViSession vi, ViAddr address, ViUInt64  val64);
 // #endif
-// 
+//
 /*- Shared Memory Operations ------------------------------------------------*/
-// 
+//
 // ViStatus _VI_FUNC  viMemAlloc      (ViSession vi, ViBusSize size, ViPBusAddress offset);
 // ViStatus _VI_FUNC  viMemFree       (ViSession vi, ViBusAddress offset);
-// 
+//
 // #if defined(_VI_INT64_UINT64_DEFINED)
 // ViStatus _VI_FUNC  viMemAllocEx    (ViSession vi, ViBusSize size, ViPBusAddress64 offset);
 // ViStatus _VI_FUNC  viMemFreeEx     (ViSession vi, ViBusAddress64 offset);
 // #endif
-// 
+//
 /*- Interface Specific Operations -------------------------------------------*/
-// 
+//
 // ViStatus _VI_FUNC  viGpibControlREN(ViSession vi, ViUInt16 mode);
 // ViStatus _VI_FUNC  viGpibControlATN(ViSession vi, ViUInt16 mode);
 // ViStatus _VI_FUNC  viGpibSendIFC   (ViSession vi);
 // ViStatus _VI_FUNC  viGpibCommand   (ViSession vi, ViBuf cmd, ViUInt32 cnt, ViPUInt32 retCnt);
 // ViStatus _VI_FUNC  viGpibPassControl(ViSession vi, ViUInt16 primAddr, ViUInt16 secAddr);
-// 
+//
 // ViStatus _VI_FUNC  viVxiCommandQuery(ViSession vi, ViUInt16 mode, ViUInt32 cmd,
 //                                      ViPUInt32 response);
 // ViStatus _VI_FUNC  viAssertUtilSignal(ViSession vi, ViUInt16 line);
 // ViStatus _VI_FUNC  viAssertIntrSignal(ViSession vi, ViInt16 mode, ViUInt32 statusID);
-// ViStatus _VI_FUNC  viMapTrigger    (ViSession vi, ViInt16 trigSrc, ViInt16 trigDest, 
+// ViStatus _VI_FUNC  viMapTrigger    (ViSession vi, ViInt16 trigSrc, ViInt16 trigDest,
 //                                     ViUInt16 mode);
 // ViStatus _VI_FUNC  viUnmapTrigger  (ViSession vi, ViInt16 trigSrc, ViInt16 trigDest);
 // ViStatus _VI_FUNC  viUsbControlOut (ViSession vi, ViInt16 bmRequestType, ViInt16 bRequest,
@@ -245,5 +245,5 @@ extern {
 //                                     ViPBuf buf, ViPUInt16 retCnt);
 // ViStatus _VI_FUNC  viPxiReserveTriggers(ViSession vi, ViInt16 cnt, ViAInt16 trigBuses,
 //                                     ViAInt16 trigLines, ViPInt16 failureIndex);
-// 
+//
 /*- The End -----------------------------------------------------------------*/
--- src/visatype.rs
+++ src/visatype.rs
@@ -1,37 +1,77 @@
 use std::libc::{c_char, c_void};
- 
-pub struct ViUInt64(u64);
-pub struct ViInt64(i64);
-pub struct ViUInt32(u32);
-pub struct ViInt32(i32);
-pub struct ViUInt16(u16);
-pub struct ViInt16(i16);
-pub struct ViUInt8(u8);
-pub struct ViInt8(i8);
-pub struct ViChar(c_char);
-pub struct ViByte(u8);
-pub struct ViAddr(~c_void);
-pub struct ViReal32(f32);
-pub struct ViReal64(f64);
-pub struct ViBuf(u8);
-pub struct ViString(~c_char);
-pub struct ViRsrc(~c_char);
-pub struct ViBoolean(u16);
-pub struct ViStatus(i32);
-pub struct ViVersion(u32);
-pub struct ViObject(u32);
-pub struct ViSession(u32);
-pub struct ViAttr(u32);
-pub struct ViEvent(u32);
-pub struct ViFindList(u32);
-pub struct ViBusAddress(u32);
-pub struct ViBusSize(u32);
-pub struct ViAttrState(u32);
-pub struct ViBusAddress64(u64);
-pub struct ViEventType(u32);
-pub struct ViKeyId(~c_char);
-pub struct ViJobId(u32);
-pub struct ViAccessMode(u32);
-pub struct ViEventFilter(u32);
-// struct ViVAList(????);
-// C functions with variable arguments not supported in 0.8 (but in 0.9pre).
+
+// from visatype.h
+type ViUInt64 = u64;
+type ViInt64 = i64;
+type ViUInt32 = u32;
+type ViInt32 = i32;
+type ViUInt16 = u16;
+type ViInt16 = i16;
+type ViUInt8 = u8;
+type ViInt8 = i8;
+type ViChar = c_char;
+type ViByte = u8;
+
+type ViAddr = ~c_void;
+
+type ViReal32 = f32;
+type ViReal64 = f64;
+
+type ViBuf = u8;
+type ViString = ~c_char;
+type ViRsrc = ~c_char;
+
+type ViBoolean = u16;
+type ViStatus = i32;
+type ViVersion = u32;
+type ViObject = u32;
+type ViSession = u32;
+
+type ViAttr = u32;
+type ViConstString = ~c_char;
+//
+
+// from visa.h
+type ViEvent = ViObject;
+// type ViEvent      _VI_PTR ViPEvent;
+type ViFindList = ViObject;
+// type ViFindList   _VI_PTR ViPFindList;
+
+#[cfg(target_pointer_width = "64")] {
+    type ViBusAddress = ViUInt64;
+    type ViBusSize = ViUInt64;
+    type ViAttrState = ViUInt64;
+}
+
+#[cfg(target_pointer_width = "32")] {
+    type ViBusAddress = ViUInt32;
+    type ViBusSize = ViUInt32;
+    type ViAttrState = ViUInt32;
+}
+
+#[cfg(target_pointer_width = "64")] {
+    type ViBusAddress64 = ViUInt64;
+// type ViBusAddress64 _VI_PTR ViPBusAddress64;
+}
+
+type ViEventType = ViUInt32;
+// type ViEventType  _VI_PTR ViPEventType;
+// type ViEventType  _VI_PTR ViAEventType;
+type void         _VI_PTR ViPAttrState;
+// type ViAttr       _VI_PTR ViPAttr;
+// type ViAttr       _VI_PTR ViAAttr;
+
+type ViKeyId = ViString;
+type ViPKeyId = ViPString;
+type ViJobId = ViUInt32;
+// type ViJobId      _VI_PTR ViPJobId;
+type ViAccessMode = ViUInt32;
+//type ViAccessMode _VI_PTR ViPAccessMode;
+//type ViBusAddress _VI_PTR ViPBusAddress;
+type ViEventFilter = ViUInt32;
+
+// typedef va_list              ViVAList;
+//
+// typedef ViStatus (_VI_FUNCH _VI_PTR ViHndlr)
+//    (ViSession vi, ViEventType eventType, ViEvent event, ViAddr userHandle);
+// C functions with variable arguments not supported in 0.8  = but in 0.9pre).
